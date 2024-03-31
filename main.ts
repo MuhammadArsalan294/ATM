e62e2f36a617e258f50efb7f8a0f68ca4c20248f
unknown <pricearslan294@gamil.com>
2024-03-31 06:40:07 +0500

    Push My ATM Machine to Github

--- main.ts
+++ main.ts
@@ -5,35 +5,41 @@ import inquirer from "inquirer";
 let myBalance = 10000; // Dollar
 let myPin = 1234;
 
-let pinAnswer = await inquirer.prompt([
+let pinAnswer = await inquirer.prompt(
+    [
         {
             name: "pin",
             message: "enter your pin",
             type: "number"
         }
-]);
+    ]
+);
 
 // Conditional Statement True 0R False
 if (pinAnswer.pin === myPin) {
     console.log("Correct pin code!!!");
 
-    let operationAns = await inquirer.prompt([
-        {
-            name: "operation",
-            message: "please select option",
-            type: "list",
-            choices: ["withdraw", "check balance"]
-        }
-    ]);
+    let operationAns = await inquirer.prompt(
+        [
+           {
+                name: "operation",
+                message: "please select option",
+                type: "list",
+                choices: ["withdraw", "check balance"]
+           }
+        ]
+    );
 
     if (operationAns.operation === "withdraw") {
-        let amountAns = await inquirer.prompt([
-            {
-                name: "amount",
-                message: "enter your amount",
-                type: "number"
-            }
-    ]);
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "enter your amount",
+                    type: "number"
+                }
+            ]    
+        );
 
         // =, -=, +=
         myBalance -= amountAns.amount;
